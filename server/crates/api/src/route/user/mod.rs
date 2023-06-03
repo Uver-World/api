a26b41c9a1345788e0e75dd641a17720e7d24a79
RqndomHax <paul.codepro@gmail.com>
2023-06-03 09:43:48 +0200

    :white_check_mark: Added user email_exists route tests

--- server/crates/api/src/route/user/mod.rs
+++ server/crates/api/src/route/user/mod.rs
@@ -1,11 +1,8 @@
-use database::Database;
-use rocket::{http::Status, response::status::Custom, serde::json::Json, *};
-use rocket_okapi::openapi;
-
 mod helper;
 
 mod route_delete_from_id;
 mod route_delete_from_token;
+mod route_email_exists;
 mod route_from_id;
 mod route_from_token;
 mod route_register;
@@ -15,25 +12,10 @@ mod route_update_auth;
 
 pub use route_delete_from_id::*;
 pub use route_delete_from_token::*;
+pub use route_email_exists::*;
 pub use route_from_id::*;
 pub use route_from_token::*;
 pub use route_register::*;
 pub use route_renew::*;
 pub use route_update::*;
 pub use route_update_auth::*;
-
-/// Check if an email is registered or not
-#[openapi(tag = "Users")]
-#[get("/email_exists/<email>")] // <- route attribute
-pub async fn email_exists(
-    database: &State<Database>,
-    email: String,
-) -> Custom<Result<Json<bool>, String>> {
-    match database.user_manager.email_exists(email).await {
-        Ok(value) => Custom(Status::Ok, Ok(Json(value))),
-        _ => Custom(
-            Status::InternalServerError,
-            Err("Database error.".to_string()),
-        ),
-    }
-}
